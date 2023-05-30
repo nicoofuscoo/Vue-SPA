a4e10b0db008ecc29b0391bffb8fc536abf8a4db
nico.fusco <nico.fusco@students.plymouth.ac.uk>
2023-05-30 15:57:21 +0100

    finished for the day

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <h1>{{ title }}</h1>
+    <Modal />
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Modal from "./components/Modal";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: { Modal },
+  data() {
+    return {
+      title: "My First Vue App",
+    };
+  },
+
+  methods: {
+    handleClick() {
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    },
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 2px solid black;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
